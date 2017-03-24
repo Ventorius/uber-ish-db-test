dc6377f917d21d835ace36d01b759620b3cf6701
Sebastian Basznianin <s.basznianin@konmex.com>
2017-03-24 14:35:07 +0100

    Finished boilerplate code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-app.use(bodyParser);
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/uber-ish');
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
